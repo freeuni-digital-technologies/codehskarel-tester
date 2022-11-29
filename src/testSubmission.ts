aefa3e8fd0ef0c2af80414a629148bde3e3999fb
ia rigby <marvinzem@gmail.com>
2022-11-29 09:57:03 +0100

    create a command line program

--- src/testSubmission.ts
+++ src/testSubmission.ts
@@ -1,12 +1,15 @@
+#!/usr/bin/env node
+
 import { KarelTester} from "./karelTester";
 import { program } from 'commander'
+import * as path from "path";
 
 program
     .requiredOption('-t, --test-file <testFile>', 'test file')
     .requiredOption('-f, --file <file>', 'submission file')
 program.parse(process.argv)
-const tester = new KarelTester(program.testFile)
-const res = tester.testSubmission(program.file)
+const tester = new KarelTester(path.resolve(process.cwd(), program.testFile))
+const res = tester.testSubmission(path.resolve(process.cwd(), program.file))
 
 if (process.send) {
     process.send(res)
